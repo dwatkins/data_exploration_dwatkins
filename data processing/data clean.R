585a3362f4e425158f6242270f60332e2501596b
dwatkins <dwatkins@seattleu.edu>
2022-08-01 18:50:05 -0500

    make lots of updates!

--- data processing/data clean.R
+++ data processing/data clean.R
@@ -9,9 +9,8 @@ trends_data <- csv_list_trendfiles %>%
 
 #get the date data
 trends_data$monthorweek <- trends_data$monthorweek %>% str_sub(start = 1, end = 10) %>% ymd() %>% floor_date('month') 
-  
 
-#COME BACK TO THIS - idk if I did it right
+
 trends_data <- trends_data %>% group_by(schname, keyword) %>% mutate(index = (index - mean(index))/sd(index))
 
 trends_data <- trends_data %>% group_by(schname, monthorweek) %>% summarize(index)
@@ -37,3 +36,17 @@ trends_data <- inner_join(trends_data, scorecard_data, by = "unitid")
 #filter for predominantly bachelor's degree awarding institutions
 trends_data <- trends_data %>% filter(PREDDEG == 3)
 
+#remove na values for index
+trends_data <- trends_data %>% filter(!is.na(index))
+
+#create true/false (1/0) variable for if the scorecard exists
+trends_data <- trends_data %>% mutate(scorecard_exist = ifelse(monthorweek >= "2015-09-01", 1, 0))
+
+#move scorecard exist and median earnings from end to behind index
+trends_data <- trends_data %>% relocate(c(scorecard_exist, `md_earn_wne_p10-REPORTED-EARNINGS`), .after = index)
+
+#remove null and privacysuppresses values from median earnings
+trends_data <- trends_data %>% subset(`md_earn_wne_p10-REPORTED-EARNINGS` != "PrivacySuppressed" & `md_earn_wne_p10-REPORTED-EARNINGS` != "NULL")
+
+#convert median earnings to numeric
+trends_data$`md_earn_wne_p10-REPORTED-EARNINGS` <- as.numeric(trends_data$`md_earn_wne_p10-REPORTED-EARNINGS`)
